d485433a09161b35377d0592e76134a247f1fc1f
anamiau <anaclara.hahn1@gmail.comgit config user.email anaclara.hahn1@gmail.com>
2023-08-11 11:51:11 -0300

    Formatação código

--- livraria/livraria/admin.py
+++ livraria/livraria/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from django.contrib import admin 
-from .models import Categoria , Editora
+from django.contrib import admin
+from .models import Categoria, Editora
+
 admin.site.register(Categoria)
 admin.site.register(Editora)
